a2445542e73256f34080354b87ea664f1492893b
daniya222 <daniyarizqi@gmail.com>
2024-01-09 23:47:37 +0800

    first section

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -26,6 +26,12 @@ function no_probs_body_classes( $classes ) {
 }
 add_filter( 'body_class', 'no_probs_body_classes' );
 
+function generate_button($url, $class, $text) {
+    echo '<a href="' . esc_url($url) . '" class="' . esc_attr($class) . '"><svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0_1_222)"><path d="M5.5 15L15.5 5" stroke="#161D25" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/><path d="M7.375 5H15.5V13.125" stroke="#161D25" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></g><defs>
+<clipPath id="clip0_1_222"><rect width="20" height="20" fill="white" transform="translate(0.5)"/></clipPath>
+</defs></svg>' . esc_html($text) . '</a>';
+}
+
 /**
  * Add a pingback url auto-discovery header for single posts, pages, or attachments.
  */
--- template-home.php
+++ template-home.php
@@ -16,12 +16,65 @@ $bannerUrl = $banner['url'];
         </div>    
   </div>
   <div class="banner-continer-holder position-relative">
-        <div class="container">
-            <div class="row">
+        <div class="container banner-text h-100">
+            <div class="row h-100">
+              <div class="col-md-6 col-left">
+                <div></div>
+                <div class="margin-bottom-9-3"><h1><?php echo get_field('title_banner');?></h1></div>
+                <div class="google-award">
+                  <div>
+                    <img width="60" src="<?php echo get_template_directory_uri(); ?>/inc/img/google.png">
+                  </div>
+                  <div>
+                    <img width="98" src="<?php echo get_template_directory_uri(); ?>/inc/img/stars.png">
+                    
+                  </div>
+                  <div class="padding-bottom-0-5">
+                    <span class="number">4.9/</span>5.0
+                    
+                  </div>
+                  <div class="rating">
+                    Google Rating
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6 col-banner-right">
+                <div class="margin-bottom-3">
+                  <?php the_content() ?>
+                </div>
+                <div class="button-holder">
+                <?php if( have_rows('button') ):
+
+                
+                  while ( have_rows('button') ) : the_row();
+                  $url = get_sub_field('button_url');
+                  $class = get_sub_field('button_class');
+                  $text = get_sub_field('button_text');
+
+                   generate_button($url, $class, $text)
+                ?> 
+                
+
+                 
+
+                <?php  
+                endwhile;
+
+            
+
+                
+
+                endif;?>
+
+                </div>
+
+              </div>
             </div>      
         </div> 
     </div>
+    
 </section>
+
 <?php
 
 get_footer();
